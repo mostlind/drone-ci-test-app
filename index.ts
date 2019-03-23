387e4cf1f0d8c264d474577e6608d840ca6a942a
Matt <mostlind@gmail.com>
2019-03-23 17:36:46 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { createServer } from "http";
+
+createServer((req, res) => {
+  res.statusCode = 200;
+  res.write("hello");
+  res.end();
+}).listen(8080);
+
+console.log("listening on 8080");
